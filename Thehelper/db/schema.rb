2271dce5ceee3b95036f60e1aee7fc31fdc40b4d
Rawan991 <rawan.alrajeh@gmail.com>
2020-04-09 07:22:48 +0300

    name is unique

--- Thehelper/db/schema.rb
+++ Thehelper/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_07_030150) do
+ActiveRecord::Schema.define(version: 2020_04_09_041732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -133,8 +133,11 @@ ActiveRecord::Schema.define(version: 2020_04_07_030150) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "coname"
+    t.index ["coname"], name: "index_users_on_coname", unique: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
